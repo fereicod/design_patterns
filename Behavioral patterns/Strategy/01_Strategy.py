fd8c6bb37f8e118ee2e23bf9ec772c51f39af16b
Fernando Espino <mferna.92@gmail.com>
2025-09-13 11:15:14 -0600

    Add example Strategy

---
+++ Behavioral patterns/Strategy/01_Strategy.py
@@ -0,0 +1,32 @@
+"""
+This pattern allows defining a family algorithms, putting each in a separatec lass, and making them interchangeable.
+- OOP -> Polymorphism
+- SOLID -> SRP, OCP, DIP
+"""
+from abc import ABC, abstractmethod
+
+
+class SortingStrategy(ABC):
+	@abstractmethod
+	def sort(self, data: list) -> list: pass
+
+
+class SortingContext:
+	def __init__(self, strategy: SortingStrategy):
+		self._strategy = strategy
+
+	def execute(self, data: list) -> list:
+		return self._strategy.sort(data)
+
+
+class BubbleSortStrategy(SortingStrategy):
+	def sort(self, data: list) -> list:
+		print("- Sorting with Bubble Sort...")
+		return sorted(data)
+
+
+print("--- Examples ---")
+bubble_sort = BubbleSortStrategy()
+context = SortingContext(bubble_sort)
+result = context.execute([5,2,8])
+print(result)
