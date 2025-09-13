2ddd899197dea91a68e6250ffeb1d344d56e08bb
Fernando Espino <mferna.92@gmail.com>
2025-09-13 11:35:26 -0600

    Update example Strategy add comments

--- Behavioral patterns/Strategy/01_Strategy.py
+++ Behavioral patterns/Strategy/01_Strategy.py
@@ -6,12 +6,16 @@ This pattern allows defining a family algorithms, putting each in a separatec la
 from abc import ABC, abstractmethod
 
 
+# DIP: This is the abstraction everyone depends on.
 class SortingStrategy(ABC):
 	@abstractmethod
 	def sort(self, data: list) -> list: pass
 
 
+# SRP: It's only responsability is to coordinate.
+# OCP: This class is CLOSED for change.
 class SortingContext:
+	# DIP: It depends on the abstraction, not a concrete strategy.
 	def __init__(self, strategy: SortingStrategy):
 		self._strategy = strategy
 
@@ -28,6 +32,8 @@ class SortingContext:
 		print(result)
 
 
+# SRP: These classes only resopnsability is to sort with their methods.
+# OCP: The system is OPEN to new strategies.
 class BubbleSortStrategy(SortingStrategy):
 	def __init__(self): print("= BubbleSort =")
 
